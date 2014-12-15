f2a21f748b639b44e22a7e1890ef3ff044b8c182
Rob Allen <rob@akrabat.com>
2014-12-15 09:16:26 +0000

    Optionally register a controller with the container.

--- public/index.php
+++ public/index.php
@@ -8,6 +8,14 @@ $app = new \App\Slim(
     ]
 );
 
+// Optionally register a controller with the container
+$app->container->singleton('App\Controller\Home', function ($container) {
+    // Retrieve any required dependencies from the container and
+    // inject into the constructor of the controller
+    return new \App\Controller\Home();
+});
+
+// Set up routes
 $app->get('/', 'App\Controller\Home:index');
 $app->get('/hello/:name', 'App\Controller\Home:hello');
 
